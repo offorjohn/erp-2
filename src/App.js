8ab9be0dd59b209a80dc2c0589ddfa5efb987e8d
offor <offorchukwuebuka8@gmail.com>
2024-06-04 15:08:26 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
-import { ColorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
 
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-      <div className="app">
-         <main className="content">
-             <Topbar/>
-         </main>
-      
-      </div>
-
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+             
+            </Routes>
+          </main>
+        </div>
       </ThemeProvider>
-     
-
     </ColorModeContext.Provider>
-   
   );
 }
 
