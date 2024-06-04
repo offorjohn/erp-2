bd07c1cc79b0fcb0f838a2848ff40da8a52b4ce1
offor <offorchukwuebuka8@gmail.com>
2024-06-04 20:44:07 +0100

    5  commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
+import Communication from "./scenes/communication";
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
@@ -22,6 +23,7 @@ function App() {
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="/communication" element={< Communication/>} />
              
             </Routes>
           </main>
